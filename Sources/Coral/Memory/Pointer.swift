06f7f5357d63889dbd793aa8f9da3fc337249863
sonodima <sonodimaa@icloud.com>
2024-06-03 11:34:10 +0200

    add pointer types documentation

--- Sources/Coral/Memory/Pointer.swift
+++ Sources/Coral/Memory/Pointer.swift
@@ -12,85 +12,200 @@
 // You should have received a copy of the GNU General Public License along with Coral.
 // If not, see <https://www.gnu.org/licenses/>.
 
-// TODO: Consider adding support for pointer chaining: Pointer<Pointer<T>>
-//       This would probably require a runtime check though, so we should benchmark it!
+public protocol __SomePointer {
+  associatedtype Pointee
+}
 
-public struct Pointer<T> {
+/// A typed pointer to an instance of `T` within a memory view.
+///
+/// To avoid undefined behavior, the type `T` must be a trivial type that can be safely
+/// copied to a raw memory buffer.
+public struct Pointer<T>: __SomePointer {
+  public typealias Pointee = T
+
+  /// The internal raw pointer that the pointer wraps.
   public var raw: RawPointer
 
+  /// The memory view that the pointer is associated with.
+  @inlinable
+  @inline(__always)
+  public var view: any MemView {
+    raw.view
+  }
+
+  /// The address of the memory location that the pointer points to.
+  @inlinable
+  @inline(__always)
   public var address: UInt {
     raw.address
   }
 
+  /// A Boolean value indicating whether the pointer is null.
+  @inlinable
+  @inline(__always)
   public var isZero: Bool {
     raw.isZero
   }
 
+  /// Creates an instance from the given `raw` pointer.
+  @inlinable
+  @inline(__always)
   public init(raw: RawPointer, for type: T.Type = T.self) {
+    assert(_isPOD(type), "Pointers to non-trivial types are unsafe and not supported!")
+
     self.raw = raw
   }
 
+  /// Creates an instance pointing to the specified `address` in `view`.
+  @inlinable
+  @inline(__always)
   public init(view: any MemView, to address: UInt, for type: T.Type = T.self) {
-    self.raw = RawPointer(view: view, to: address)
+    assert(_isPOD(type), "Pointers to non-trivial types are unsafe and not supported!")
+
+    raw = RawPointer(view: view, to: address)
   }
 
-  public func read() -> T? {
+  /// Reads the value from the memory at the pointed location.
+  ///
+  /// - Returns: The value read from memory if successful; otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func deref() -> T? {
     raw.read(as: T.self)
   }
 
+  /// Writes the given `value` to the memory at the pointed location.
+  ///
+  /// - Returns: `true` if the value was successfully written to memory;
+  ///            otherwise, `false`.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func write(value: T) -> Bool {
     raw.write(value: value, as: T.self)
   }
 
-  public func add(_ value: UInt) -> Self {
-    Self(raw: raw.add(value))
-  }
-
-  public func sub(_ value: UInt) -> Self {
-    Self(raw: raw.sub(value))
-  }
-
-  public func offset(_ value: Int) -> Self {
+  /// Returns a ``Pointer`` to the instance of `T` pointed to by `self`, offset by
+  /// `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func offset(_ value: Int) -> Pointer {
     Self(raw: raw.offset(value))
   }
 
-  public static func + (lhs: Self, rhs: UInt) -> Self {
+  /// Returns a ``Pointer`` to the instance of `T` pointed to by `self`, incremented by
+  /// `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func adding(_ value: UInt) -> Pointer {
+    Self(raw: raw.adding(value))
+  }
+
+  /// Returns a ``Pointer`` to the instance of `T` pointed to by `self`, decremented by
+  /// `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func subtracting(_ value: UInt) -> Pointer {
+    Self(raw: raw.subtracting(value))
+  }
+
+  /// Adds `value` to the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public mutating func add(_ value: UInt) {
+    raw.add(value)
+  }
+
+  /// Subtracts `value` from the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public mutating func subtract(_ value: UInt) {
+    raw.subtract(value)
+  }
+
+  /// Returns a ``Pointer`` to the instance of `T` pointed to by `self`, incremented by
+  /// `rhs` bytes.
+  @inlinable
+  @inline(__always)
+  public static func + (lhs: Pointer, rhs: UInt) -> Pointer {
+    lhs.adding(rhs)
+  }
+
+  /// Returns a ``Pointer`` to the instance of `T` pointed to by `self`, decremented by
+  /// `rhs` bytes.
+  @inlinable
+  @inline(__always)
+  public static func - (lhs: Pointer, rhs: UInt) -> Pointer {
+    lhs.subtracting(rhs)
+  }
+
+  /// Adds `rhs` to the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public static func += (lhs: inout Pointer, rhs: UInt) {
     lhs.add(rhs)
   }
 
-  public static func - (lhs: Self, rhs: UInt) -> Self {
-    lhs.sub(rhs)
+  /// Subtracts `rhs` from the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public static func -= (lhs: inout Pointer, rhs: UInt) {
+    lhs.subtract(rhs)
   }
+}
 
-  public static func += (lhs: inout Self, rhs: UInt) {
-    lhs.raw += rhs
+extension Pointer where T == RawPointer {
+  /// Reads the value from the memory at the pointed location.
+  ///
+  /// - Returns: The value read from memory if successful; otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func deref() -> T? {
+    raw.deref()
   }
+}
 
-  public static func -= (lhs: inout Self, rhs: UInt) {
-    lhs.raw -= rhs
+extension Pointer where T: __SomePointer {
+  /// Reads the value from the memory at the pointed location.
+  ///
+  /// - Returns: The value read from memory if successful; otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func deref() -> Pointer<T.Pointee>? {
+    raw.deref(as: T.Pointee.self)
   }
 }
 
 extension Pointer: Equatable {
-  public static func == (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if the two pointers point to the same address; otherwise, `false`.
+  ///
+  /// Two pointers are considered equal if they point to the same address, regardless of
+  /// the memory view they are associated with.
+  @inlinable
+  @inline(__always)
+  public static func == (lhs: Pointer, rhs: Pointer) -> Bool {
     lhs.raw == rhs.raw
   }
 }
 
 extension Pointer: Comparable {
-  public static func < (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if `lhs` points to a lower address than `rhs`; otherwise, `false`.
+  @inlinable
+  @inline(__always)
+  public static func < (lhs: Pointer, rhs: Pointer) -> Bool {
     lhs.raw < rhs.raw
   }
 }
 
 extension Pointer: Hashable {
+  /// Hashes the essential components of the pointer into the given hasher.
   public func hash(into hasher: inout Hasher) {
     hasher.combine(raw)
   }
 }
 
 extension Pointer: CustomDebugStringConvertible {
+  /// Returns a textual representation of the pointer, suitable for debugging.
   public var debugDescription: String {
     let address = String(address, radix: 16, uppercase: true)
     return "Pointer<\(T.self)>(0x\(address))"
@@ -98,6 +213,7 @@ extension Pointer: CustomDebugStringConvertible {
 }
 
 extension Pointer: CustomStringConvertible {
+  /// Returns a textual representation of the pointer.
   public var description: String {
     let address = String(address, radix: 16, uppercase: true)
     return "0x\(address)"
--- Sources/Coral/Memory/RawPointer.swift
+++ Sources/Coral/Memory/RawPointer.swift
@@ -12,170 +12,456 @@
 // You should have received a copy of the GNU General Public License along with Coral.
 // If not, see <https://www.gnu.org/licenses/>.
 
+/// A pointer to a memory location within a memory view.
 public struct RawPointer {
+  /// The memory view that the pointer is associated with.
   public let view: any MemView
+
+  /// The address of the memory location that the pointer points to.
   public var address: UInt
 
+  /// A Boolean value indicating whether the pointer is null.
+  @inlinable
+  @inline(__always)
   public var isZero: Bool {
     address == 0
   }
 
-  public var protection: Protection? {
-    view.protection(at: address)
-  }
-
+  /// Creates an instance pointing to the specified `address` in `view`.
+  @inlinable
+  @inline(__always)
   public init(view: any MemView, to address: UInt) {
     self.view = view
     self.address = address
   }
 
-  public func deref() -> RawPointer? {
-    view.read(from: address, as: RawPointer.self)
-  }
-
-  public func deref<T>(as type: T.Type = T.self) -> Pointer<T>? {
-    view.read(from: address, as: Pointer<T>.self)
-  }
-
+  /// Copies the contents of the memory at the pointed location into `buffer`.
+  ///
+  /// - Returns: The number of bytes read from memory.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func read(into buffer: UnsafeMutableRawBufferPointer) -> UInt {
     view.read(from: address, into: buffer)
   }
 
+  /// Copies the content of `data` to the memory at the pointed location.
+  ///
+  /// - Returns: The number of bytes written to memory.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func write(data: UnsafeRawBufferPointer) -> UInt {
     view.write(to: address, data: data)
   }
 
+  /// Reads a value of type `T` from the memory at the pointed location.
+  ///
+  /// - Returns: The value read from memory if successful; otherwise, `nil`.
+  ///
+  /// To avoid undefined behavior, the type `T` must be a trivial type that can be
+  /// safely copied to a raw memory buffer.
+  @inlinable
+  @inline(__always)
   public func read<T>(as type: T.Type = T.self) -> T? {
     view.read(from: address, as: type)
   }
 
+  /// Writes the given `value` to the memory at the pointed location.
+  ///
+  /// - Returns: `true` if the value was successfully written to memory;
+  ///            otherwise, `false`.
+  ///
+  /// To avoid undefined behavior, the type `T` must be a trivial type that can be
+  /// safely copied to a raw memory buffer.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func write<T>(value: T, as type: T.Type = T.self) -> Bool {
     view.write(to: address, value: value, as: type)
   }
 
-  public func read(as type: Self.Type = Self.self) -> Self? {
+  /// Reads a ``RawPointer`` from the memory at the pointed location.
+  ///
+  /// - Returns: The pointer read from memory if successful; otherwise, `nil`.
+  ///
+  /// - SeeAlso: ``deref()``
+  @inlinable
+  @inline(__always)
+  public func read(as type: RawPointer.Type = RawPointer.self) -> RawPointer? {
     view.read(from: address, as: type)
   }
 
+  /// Writes a ``RawPointer`` to the memory at the pointed location.
+  ///
+  /// - Returns: `true` if the pointer was successfully written to memory;
+  ///            otherwise, `false`.
+  @inlinable
+  @inline(__always)
   @discardableResult
-  public func write(value: Self, as type: Self.Type = Self.self) -> Bool {
+  public func write(
+    value: RawPointer,
+    as type: RawPointer.Type = RawPointer.self
+  ) -> Bool {
     view.write(to: address, value: value, as: type)
   }
 
+  /// Reads a ``Pointer`` to an instance of `T` from the memory at the pointed location.
+  ///
+  /// - Returns: The pointer read from memory if successful; otherwise, `nil`.
+  ///
+  /// - SeeAlso: ``deref(as:)``
+  @inlinable
+  @inline(__always)
   public func read<T>(as type: Pointer<T>.Type = Pointer<T>.self) -> Pointer<T>? {
     view.read(from: address, as: type)
   }
 
+  /// Writes a ``Pointer`` to the memory at the pointed location.
+  ///
+  /// - Returns: `true` if the pointer was successfully written to memory;
+  ///            otherwise, `false`.
+  @inlinable
+  @inline(__always)
   @discardableResult
-  public func write<T>(value: Pointer<T>,
+  public func write<T>(
+    value: Pointer<T>,
     as type: Pointer<T>.Type = Pointer<T>.self
   ) -> Bool {
     view.write(to: address, value: value, as: type)
   }
 
-  public func read<T>(count: Int, of type: T.Type = T.self) -> ContiguousArray<T> {
-    view.read(from: address, count: count, of: type)
+  /// Reads an array of at most `maxCount` elements of type `T` from the memory at
+  /// the pointed location.
+  ///
+  /// - Returns: The array read from memory if successful; otherwise, an empty array.
+  ///
+  /// To avoid undefined behavior, the type `T` must be a trivial type that can be
+  /// safely copied to a raw memory buffer.
+  @inlinable
+  @inline(__always)
+  public func read<T>(maxCount: UInt, of type: T.Type = T.self) -> ContiguousArray<T> {
+    view.read(from: address, maxCount: maxCount, of: type)
   }
 
+  /// Writes the given `array` to the memory at the pointed location.
+  ///
+  /// - Returns: The number of elements written to memory.
+  ///
+  /// To avoid undefined behavior, the type `T` must be a trivial type that can be
+  /// safely copied to a raw memory buffer.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func write<T>(array: ContiguousArray<T>, of type: T.Type = T.self) -> UInt {
     view.write(to: address, array: array, of: type)
   }
 
+  /// Reads an array of at most `maxCount` pointers from the memory at the
+  /// pointed location.
+  ///
+  /// - Returns: The array read from memory if successful; otherwise, an empty array.
+  @inlinable
+  @inline(__always)
+  public func read(
+    maxCount: UInt,
+    of type: RawPointer.Type = RawPointer.self
+  ) -> ContiguousArray<RawPointer> {
+    view.read(from: address, maxCount: maxCount, of: type)
+  }
+
+  /// Writes the given `array` of pointers to the memory at the pointed location.
+  ///
+  /// - Returns: The number of elements written to memory.
+  @inlinable
+  @inline(__always)
+  @discardableResult
+  public func write(
+    array: ContiguousArray<RawPointer>,
+    of type: RawPointer.Type = RawPointer.self
+  ) -> UInt {
+    view.write(to: address, array: array, of: type)
+  }
+
+  /// Reads an array of at most `maxCount` pointers to instances of `T` from the memory
+  /// at the pointed location.
+  ///
+  /// - Returns: The array read from memory if successful; otherwise, an empty array.
+  @inlinable
+  @inline(__always)
+  public func read<T>(
+    maxCount: UInt,
+    of type: Pointer<T>.Type = Pointer<T>.self
+  ) -> ContiguousArray<Pointer<T>> {
+    view.read(from: address, maxCount: maxCount, of: type)
+  }
+
+  /// Writes the given `array` of pointers to instances of `T` to the memory at the
+  /// pointed location.
+  ///
+  /// - Returns: The number of elements written to memory.
+  @inlinable
+  @inline(__always)
+  @discardableResult
+  public func write<T>(
+    array: ContiguousArray<Pointer<T>>,
+    of type: Pointer<T>.Type = Pointer<T>.self
+  ) -> UInt {
+    view.write(to: address, array: array, of: type)
+  }
+
+  /// Reads a string of at most `maxChars` characters from the memory at the
+  /// pointed location.
+  ///
+  /// - Parameters:
+  ///   - maxChars: The maximum number of characters to read.
+  ///   - encoding: The encoding to use when decoding the string.
+  ///   - zeroTerm: Whether to trim the string at the first zero character.
+  ///
+  /// - Returns: The string read from memory if successful; otherwise, an empty string.
+  ///
+  /// When possible, it is preferable to use the `Unicode.UTF8` encoding for writing
+  /// strings, as it skips the transcoding step, which can be expensive.
+  @inlinable
+  @inline(__always)
   public func read<E: Unicode.Encoding>(
-    chars: Int,
+    maxChars: UInt,
     encoding: E.Type = Unicode.UTF8.self,
     zeroTerm: Bool = false
   ) -> String {
-    view.read(from: address, chars: chars, encoding: encoding, zeroTerm: zeroTerm)
+    view.read(from: address, maxChars: maxChars, encoding: encoding, zeroTerm: zeroTerm)
   }
 
+  /// Writes the given `string` to the memory at the pointed location.
+  ///
+  /// - Parameters:
+  ///   - string: The string to write to memory.
+  ///   - encodedAs: The encoding to use when encoding the string.
+  ///   - zeroTerm: Whether to append a zero character at the end of the string.
+  ///
+  /// - Returns: `true` if the string was successfully written to memory;
+  ///            otherwise, `false`.
+  ///
+  /// When possible, it is preferable to use the `Unicode.UTF8` encoding for writing
+  /// strings, as it skips the transcoding step, which can be expensive.
+  @inlinable
+  @inline(__always)
   @discardableResult
   public func write<E: Unicode.Encoding>(
     string: String,
-    encoding: E.Type = Unicode.UTF8.self,
+    encodedAs: E.Type = Unicode.UTF8.self,
     zeroTerm: Bool = false
   ) -> Bool {
-    view.write(to: address, string: string, encoding: encoding, zeroTerm: zeroTerm)
+    view.write(to: address, string: string, encodedAs: encodedAs, zeroTerm: zeroTerm)
   }
 
+  /// Reads a ``RawPointer`` from the memory at the pointed location.
+  ///
+  /// - Returns: The pointer read from memory if successful; otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func deref() -> RawPointer? {
+    view.read(from: address, as: RawPointer.self)
+  }
+
+  /// Reads a ``Pointer`` to an instance of `T` from the memory at the pointed location.
+  ///
+  /// - Returns: The pointer read from memory if successful; otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func deref<T>(as type: T.Type = T.self) -> Pointer<T>? {
+    view.read(from: address, as: Pointer<T>.self)
+  }
+
+  /// Frees `byteCount` bytes of memory starting from the pointed location.
+  ///
+  /// - Returns: `true` if the memory was successfully freed; otherwise, `false`.
+  ///
+  /// - Important: Some platforms do not support specifying the number of bytes to free.
+  ///              In such cases, the entire memory allocation will be freed.
+  @inlinable
+  @inline(__always)
   @discardableResult
-  public func free(size: UInt) -> Bool {
-    view.free(from: address, size: size)
+  public func free(byteCount: UInt) -> Bool {
+    view.free(from: address, byteCount: byteCount)
   }
 
+  /// Changes the protection level of the memory region at the pointed location.
+  ///
+  /// - Parameters:
+  ///   - byteCount: The number of bytes to protect.
+  ///   - value: The new protection level to apply.
+  ///
+  /// - Returns: `true` if the protection level was successfully changed;
+  ///            otherwise, `false`.
+  ///
+  /// - Important: Some platforms restrict the ability to have pages that are both
+  ///              writable and executable at the same time.
+  @inlinable
+  @inline(__always)
   @discardableResult
-  public func protect(size: UInt, value: Protection) -> Bool {
-    view.protect(at: address, size: size, value: value)
+  public func protect(byteCount: UInt, value: Protection) -> Bool {
+    view.protect(at: address, byteCount: byteCount, value: value)
   }
 
+  /// The protection level of the memory at the pointed location, or `nil` if it cannot
+  /// be determined.
+  @inlinable
+  @inline(__always)
+  public var protection: Protection? {
+    view.protection(at: address)
+  }
+
+  /// Returns the pointer as a typed pointer to an instance of `T`.
+  @inlinable
+  @inline(__always)
+  public func typed<T>(as type: T.Type = T.self) -> Pointer<T> {
+    Pointer(raw: self, for: type)
+  }
+
+  /// Converts this pointer to another type, using the provided constructor function.
+  ///
+  /// If the target type `T` has a constructor or a factory method that accepts a single
+  /// ``Pointer`` argument, you can use the following simplified syntax:
+  ///
+  /// ```swift
+  /// ptr.into(Player.init)
+  /// ```
+  ///
+  /// This approach to conversion is very flexible, because it even allows the usage
+  /// of fallible constructor methods, that may return `nil`.
+  ///
+  /// ```swift
+  /// ptr.into(Player.tryFromPointer)?.health
+  /// ```
+  @inlinable
+  @inline(__always)
+  public func to<T>(_ lambda: (RawPointer) -> T) -> T {
+    lambda(self)
+  }
+
+  /// Returns a ``MemRange`` starting at the memory location pointed to by `self` and
+  /// spanning `size` bytes.
+  @inlinable
+  @inline(__always)
   public func toRange(size: UInt) -> MemRange {
     MemRange(base: self, size: size)
   }
 
-  public func toRange(end: Self) -> MemRange {
-    MemRange(base: self, size: end.address >= address ? end.address - address : 0)
+  /// Creates a ``MemRange`` starting at the memory location pointed to by `self` and
+  /// spanning up to the memory location pointed to by `end`.
+  /// 
+  /// - Returns: The memory range if `end` is greater than or equal to `self`;
+  ///            otherwise, `nil`.
+  @inlinable
+  @inline(__always)
+  public func toRange(end: RawPointer) -> MemRange? {
+    if end >= self {
+      MemRange(base: self, size: end.address - address)
+    } else {
+      nil
+    }
   }
 
-  public func typed<T>(_ type: T.Type = T.self) -> Pointer<T> {
-    Pointer(raw: self, for: type)
+  /// Returns a ``RawPointer`` to the memory location pointed to by `self`, offset by
+  /// `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func offset(_ value: Int) -> RawPointer {
+    RawPointer(view: view, to: address &+ UInt(bitPattern: value))
   }
 
-  public func to<T>(_ lambda: (Self) -> T) -> T {
-    lambda(self)
+  /// Returns a ``RawPointer`` to the memory location pointed to by `self`, incremented
+  /// by `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func adding(_ value: UInt) -> RawPointer {
+    RawPointer(view: view, to: address &+ value)
   }
 
-  public func add(_ value: UInt) -> Self {
-    Self(view: view, to: address &+ value)
+  /// Returns a ``RawPointer`` to the memory location pointed to by `self`, decremented
+  /// by `value` bytes.
+  @inlinable
+  @inline(__always)
+  public func subtracting(_ value: UInt) -> RawPointer {
+    RawPointer(view: view, to: address &- value)
   }
 
-  public func sub(_ value: UInt) -> Self {
-    Self(view: view, to: address &- value)
+  /// Adds `value` to the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public mutating func add(_ value: UInt) {
+    address += value
   }
 
-  public func offset(_ value: Int) -> Self {
-    Self(view: view, to: address &+ UInt(bitPattern: value))
+  /// Subtracts `value` from the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public mutating func subtract(_ value: UInt) {
+    address -= value
   }
 
-  public static func + (lhs: Self, rhs: UInt) -> Self {
+  /// Returns a ``RawPointer`` to the memory location pointed to by `self`, incremented
+  /// by `rhs` bytes.
+  @inlinable
+  @inline(__always)
+  public static func + (lhs: RawPointer, rhs: UInt) -> RawPointer {
+    lhs.adding(rhs)
+  }
+
+  /// Returns a ``RawPointer`` to the memory location pointed to by `self`, decremented
+  /// by `rhs` bytes.
+  @inlinable
+  @inline(__always)
+  public static func - (lhs: RawPointer, rhs: UInt) -> RawPointer {
+    lhs.subtracting(rhs)
+  }
+
+  /// Adds `rhs` to the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public static func += (lhs: inout RawPointer, rhs: UInt) {
     lhs.add(rhs)
   }
 
-  public static func - (lhs: Self, rhs: UInt) -> Self {
-    lhs.sub(rhs)
-  }
-
-  public static func += (lhs: inout Self, rhs: UInt) {
-    lhs.address &+= rhs
-  }
-
-  public static func -= (lhs: inout Self, rhs: UInt) {
-    lhs.address &-= rhs
+  /// Subtracts `rhs` from the address of the pointer.
+  @inlinable
+  @inline(__always)
+  public static func -= (lhs: inout RawPointer, rhs: UInt) {
+    lhs.subtract(rhs)
   }
 }
 
 extension RawPointer: Equatable {
-  public static func == (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if the two pointers point to the same address; otherwise, `false`.
+  /// 
+  /// Two pointers are considered equal if they point to the same address, regardless of
+  /// the memory view they are associated with.
+  @inlinable
+  @inline(__always)
+  public static func == (lhs: RawPointer, rhs: RawPointer) -> Bool {
     lhs.address == rhs.address
   }
 }
 
 extension RawPointer: Comparable {
-  public static func < (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if `lhs` points to a lower address than `rhs`; otherwise, `false`.
+  @inlinable
+  @inline(__always)
+  public static func < (lhs: RawPointer, rhs: RawPointer) -> Bool {
     lhs.address < rhs.address
   }
 }
 
 extension RawPointer: Hashable {
+  /// Hashes the essential components of the pointer into the given hasher.
   public func hash(into hasher: inout Hasher) {
     hasher.combine(address)
   }
 }
 
 extension RawPointer: CustomDebugStringConvertible {
+  /// Returns a textual representation of the pointer, suitable for debugging.
   public var debugDescription: String {
     let address = String(address, radix: 16, uppercase: true)
     return "RawPointer(0x\(address))"
@@ -183,6 +469,7 @@ extension RawPointer: CustomDebugStringConvertible {
 }
 
 extension RawPointer: CustomStringConvertible {
+  /// Returns a textual representation of the pointer.
   public var description: String {
     let address = String(address, radix: 16, uppercase: true)
     return "0x\(address)"
