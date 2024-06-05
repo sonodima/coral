2b713175110c744acb62141db03e70685f18560c
sonodima <sonodimaa@icloud.com>
2024-06-05 22:40:56 +0200

    added docs for timespan

--- Sources/Coral/Utils/TimeSpan.swift
+++ Sources/Coral/Utils/TimeSpan.swift
@@ -12,87 +12,150 @@
 // You should have received a copy of the GNU General Public License along with Coral.
 // If not, see <https://www.gnu.org/licenses/>.
 
+/// An unsigned period of time with nanosecond precision.
 public struct TimeSpan {
+  /// The number of nanoseconds in the time span.
   public var nanos: UInt64
 
+  /// The number of microseconds in the time span.
+  /// 
+  /// This value will always be greater than or equal to zero.
   public var micros: Double {
     Double(nanos) / 1_000
   }
 
+  /// The number of milliseconds in the time span.
+  /// 
+  /// This value will always be greater than or equal to zero.
   public var millis: Double {
     Double(nanos) / 1_000_000
   }
 
+  /// The number of seconds in the time span.
+  /// 
+  /// This value will always be greater than or equal to zero.
   public var secs: Double {
     Double(nanos) / 1_000_000_000
   }
 
+  /// Creates an instance with the specified number of nanoseconds.
   public init(nanos: UInt64) {
     self.nanos = nanos
   }
 
+  /// Creates an instance with the specified number of microseconds.
+  /// 
+  /// If `micros` is less than zero, the value will be clamped to zero.
   public init(micros: Double) {
-    self.nanos = UInt64(micros * 1_000)
+    let micros = micros < 0.0 ? 0.0 : micros
+    nanos = UInt64(micros * 1_000)
   }
 
+  /// Creates an instance with the specified number of milliseconds.
+  /// 
+  /// If `millis` is less than zero, the value will be clamped to zero.
   public init(millis: Double) {
-    self.nanos = UInt64(millis * 1_000_000)
+    let millis = millis < 0.0 ? 0.0 : millis
+    nanos = UInt64(millis * 1_000_000)
   }
 
+  /// Creates an instance with the specified number of seconds.
+  /// 
+  /// If `secs` is less than zero, the value will be clamped to zero.
   public init(secs: Double) {
-    self.nanos = UInt64(secs * 1_000_000_000)
+    let secs = secs < 0.0 ? 0.0 : secs
+    nanos = UInt64(secs * 1_000_000_000)
   }
 
-  public func add(_ other: Self) -> Self {
-    Self(nanos: UInt64.max - nanos > other.nanos ? nanos + other.nanos : UInt64.max)
+  /// Returns a ``TimeSpan`` that represents the sum of `self` and `other`.
+  public func adding(_ other: TimeSpan) -> TimeSpan {
+    TimeSpan(
+      nanos: UInt64.max - nanos > other.nanos
+        ? nanos + other.nanos : UInt64.max)
   }
 
-  public func sub(_ other: Self) -> Self {
-    Self(nanos: other.nanos > other.nanos ? other.nanos - other.nanos : 0)
+  /// Returns a ``TimeSpan`` that represents the difference between `self` and `other`.
+  public func subtracting(_ other: TimeSpan) -> TimeSpan {
+    TimeSpan(
+      nanos: other.nanos > other.nanos
+        ? other.nanos - other.nanos : 0)
   }
 
-  public static func + (lhs: Self, rhs: Self) -> Self {
+  /// Adds `other` to the time span.
+  public mutating func add(_ other: TimeSpan) {
+    if UInt64.max - nanos > other.nanos {
+      nanos += other.nanos
+    } else {
+      nanos = UInt64.max
+    }
+  }
+
+  /// Subtracts `other` from the time span.
+  public mutating func subtract(_ other: TimeSpan) {
+    if nanos > other.nanos {
+      nanos -= other.nanos
+    } else {
+      nanos = 0
+    }
+  }
+
+  // TODO: multiply, divide, multiplying, dividing, *, /, *=, /=
+
+  /// Returns a ``TimeSpan`` that represents the sum of `lhs` and `rhs`.
+  public static func + (lhs: TimeSpan, rhs: TimeSpan) -> TimeSpan {
+    lhs.adding(rhs)
+  }
+
+  /// Returns a ``TimeSpan`` that represents the difference between `lhs` and `rhs`.
+  public static func - (lhs: TimeSpan, rhs: TimeSpan) -> TimeSpan {
+    lhs.subtracting(rhs)
+  }
+
+  /// Adds `rhs` to the `lhs` time span.
+  public static func += (lhs: inout TimeSpan, rhs: TimeSpan) {
     lhs.add(rhs)
   }
 
-  public static func - (lhs: Self, rhs: Self) -> Self {
-    lhs.sub(rhs)
-  }
-
-  public static func += (lhs: inout Self, rhs: Self) {
-    lhs = lhs.add(rhs)
-  }
-
-  public static func -= (lhs: inout Self, rhs: Self) {
-    lhs = lhs.sub(rhs)
+  /// Subtracts `rhs` from the `lhs` time span.
+  public static func -= (lhs: inout TimeSpan, rhs: TimeSpan) {
+    lhs.subtract(rhs)
   }
 }
 
 extension TimeSpan: Equatable {
-  public static func == (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if the length of the two time spans are equal; otherwise, `false`.
+  @inlinable
+  @inline(__always)
+  public static func == (lhs: TimeSpan, rhs: TimeSpan) -> Bool {
     lhs.nanos == rhs.nanos
   }
 }
 
 extension TimeSpan: Comparable {
-  public static func < (lhs: Self, rhs: Self) -> Bool {
+  /// Returns `true` if `lhs` is less than `rhs`; otherwise, `false`.
+  @inlinable
+  @inline(__always)
+  public static func < (lhs: TimeSpan, rhs: TimeSpan) -> Bool {
     lhs.nanos < rhs.nanos
   }
 }
 
 extension TimeSpan: Hashable {
+  /// Hashes the essential components of the time span into the given hasher.
   public func hash(into hasher: inout Hasher) {
     hasher.combine(nanos)
   }
 }
 
 extension TimeSpan: CustomDebugStringConvertible {
+  /// A textual representation of the time span, suitable for debugging.
   public var debugDescription: String {
     String(format: "%.3fms", millis)
   }
 }
 
 extension TimeSpan: CustomStringConvertible {
+  /// A textual representation of the time span.
   public var description: String {
     String(format: "%.3fms", millis)
   }
