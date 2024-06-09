5352a316079b459d474440f05ab3e4bb08fb72b1
sonodima <sonodimaa@icloud.com>
2024-06-09 18:35:15 +0200

    fix C modulemaps

--- Sources/CWinPrivate/include/nt.h
+++ Sources/CWinPrivate/include/nt.h
@@ -1,9 +1,11 @@
-#ifndef _CWINPRIVATE_NT_H
-#define _CWINPRIVATE_NT_H
+#ifndef __CWINPRIVATE_NT_H
+#define __CWINPRIVATE_NT_H
 
-#include <ntdef.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <wincred.h>
 
-NTSYSAPI NTSTATUS NTAPI
-NtDelayExecution(IN BOOLEAN Alertable, IN PLARGE_INTEGER DelayInterval);
+NTSYSAPI NTSTATUS NTAPI NtDelayExecution(IN BOOLEAN Alertable,
+                                         IN PLARGE_INTEGER DelayInterval);
 
-#endif // _CWINPRIVATE_NT_H
+#endif // __CWINPRIVATE_NT_H
